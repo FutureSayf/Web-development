59a915873a0c331856b2576e0e309020f332a4b1
Sayf-al-Din <thefuture.sayf@gmail.com>
2021-02-03 22:21:49 +0100

    update

--- portfolio/app.js
+++ portfolio/app.js
@@ -25,3 +25,122 @@ navslide();
 //     // roep al je functies aan
 //     navslide();
 // }
+
+
+// TypeWriter effect
+let app = document.getElementById('app');
+let intro = document.getElementById('intro');
+
+let typewriter = new Typewriter(app, 
+    {
+        loop: true
+    });
+
+    let typewriter1 = new Typewriter(intro, 
+        {
+            loop: true
+        });
+
+typewriter.typeString('a Creative mind')
+    .pauseFor(1000)
+    .deleteAll()
+
+    .typeString('"Lost in Space"')
+    .pauseFor(2500)
+    .deleteAll()
+
+    .typeString('<strong>Guided</strong> by Light')
+    .pauseFor(2500)
+    .deleteAll()
+
+    .typeString('Always Learning')
+    .pauseFor(500)
+    .deleteAll()
+
+    .typeString('<strong>Dreaming</strong>')
+    .pauseFor(100)
+    .deleteChars(8)
+
+    .typeString('<strong>Breathing</strong>')
+    .pauseFor(100)
+    .deleteChars(9)
+
+    .typeString('<strong>LIVING</strong>')
+    .pauseFor(2500)
+    .deleteAll()
+
+    .typeString('I')
+    .pauseFor(3000)
+    .deleteAll()
+
+    .start();
+
+    typewriter1.typeString('Traveling True ')
+    .pauseFor(2500)
+    // .deleteChars(1)
+
+    .typeString('<strong>Time</strong>')
+    .pauseFor(2500)
+    .deleteAll()
+
+    .typeString(' <strong>Developing</strong>')
+    .pauseFor(1000)
+    .deleteChars(8)
+
+    .typeString('<strong>signing</strong>')
+    .pauseFor(1000)
+    .deleteAll()
+
+    .typeString(' Traveling True Thoughts that Run My Mind.')
+    .pauseFor(2500)
+    .deleteAll()
+
+    // .typeString('I  ')
+    // .pauseFor(500)
+    
+
+    // .typeString('<strong>AM</strong> ')
+    // .pauseFor(1500)
+
+    .typeString('LIVING "LIVE".')
+    .pauseFor(2500)
+    .deleteAll()
+
+    .pauseFor(500)
+    .typeString('I <strong>AM</strong> Future.Sayf')
+    .pauseFor(2500)
+    // .deleteAll()
+
+    .typeString(' DIGITAL')
+    .pauseFor(2500)
+    .deleteAll()
+
+    .typeString('سیف الدین')
+    .pauseFor(2500)
+    .deleteAll()
+
+    .start();
+
+// END TYPEwriter https://github.com/tameemsafi/typewriterjs
+// https://safi.me.uk/typewriterjs/   سیف الدین
+
+
+
+
+/*--/ Star Typed /--*/
+
+// function slider(){
+
+//     if ($('.text-slider').length == 1) {
+//         var typed_strings = $('.text-slider-items').text();
+//             var typed = new Typed('.text-slider', {
+//                 strings: typed_strings.split(','),
+//                 typeSpeed: 80,
+//                 loop: true,
+//                 backDelay: 1100,
+//                 backSpeed: 30
+//             });
+//         }
+
+// }
+// slider();
