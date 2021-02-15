9a14c32004bf0daf5d4a3011568179cdd9d4d41b
Future.Sayf <thefuture.sayf@gmail.com>
2021-02-15 16:03:02 +0100

    React app update

--- React/my-app/src/App.js
+++ React/my-app/src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Hello, Reacht app</h1>
+        <p>Het werkt!</p>
+        <Person />
       </div>
     );
     // return React.createElement('div', null, React.createElement('h1', {className: 'App'}, 'does this work now?'));
