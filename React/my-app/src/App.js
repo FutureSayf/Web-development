a16f1c761dfbae1cffd3fa56501b18d36dc946b1
Future.Sayf <thefuture.sayf@gmail.com>
2021-02-16 16:43:42 +0100

    Update

--- React/my-app/src/App.js
+++ React/my-app/src/App.js
@@ -2,20 +2,39 @@ import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-class App extends Component {
-  state = {
+class App extends Component 
+{
+  state = 
+  {
     persons: [
       { name: 'Max', age: 28},
       { name: 'Mathew', age: 26},
       { name: 'Marvin', age: 8}
-    ]
+    ],
+    otherState: 'some other date'
   }
-  render() {
+
+  switchNameHandler = () =>
+  {
+    // 
+    // Don't Do THIS/ this.state.persons[0].name = 'Anthony';
+    this.setState(
+    {
+      persons: 
+    [
+      { name: 'Anthony', age: 28},
+      { name: 'Mathew', age: 26},
+      { name: 'Marvin', age: 18}
+    ]
+    })
+  }
+  render() 
+  {
     return (
       <div className="App">
         <h1>Hello, React app</h1>
         <p>Het werkt!</p>
-        <button>Switch Name</button>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
         <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
         <Person name={this.state.persons[1].name} age={this.state.persons[1].age} >My Hobbies: Racing</Person>
         <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
