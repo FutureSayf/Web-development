003056aa640cb451a503534832f73934ca161b3e
Future.Sayf <thefuture.sayf@gmail.com>
2021-02-16 11:36:05 +0100

    React app video 39. props & state

--- React/my-app/src/App.js
+++ React/my-app/src/App.js
@@ -3,12 +3,22 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: 'Max', age: 28},
+      { name: 'Mathew', age: 26},
+      { name: 'Marvin', age: 8}
+    ]
+  }
   render() {
     return (
       <div className="App">
-        <h1>Hello, Reacht app</h1>
+        <h1>Hello, React app</h1>
         <p>Het werkt!</p>
-        <Person />
+        <button>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} >My Hobbies: Racing</Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
       </div>
     );
     // return React.createElement('div', null, React.createElement('h1', {className: 'App'}, 'does this work now?'));
