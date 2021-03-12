0d50f0d71dd725a80e9f9a9e388782dcc59524b2
Future.Sayf <thefuture.sayf@gmail.com>
2021-03-12 17:23:51 +0100

    Update WeatherApp

--- React/react-projectguide/src/App.js
+++ React/react-projectguide/src/App.js
@@ -2,59 +2,27 @@ import React, { useState, useEffect } from 'react'
 import './App.css';
 
 import {getWeatherData} from './Components/WeatherInfo/WeatherInfo';
-
 import WeatherCard from './Components/WeatherCard/WeatherCard'
 
 
 
 
- const App = () => {
-   
-  const [weatherdata, setWeatherData] = useState(null);
-  const [city, setCity] = useState('istanbul');
-  const [loading, setLoading] = useState(false);
-  
-  const getData = async () => {
-    try{
-      const data = await getWeatherData(city);
-      setWeatherData(data);
-      console.log(data);
-    }catch(error){
-      console.log(error.message);
-    }
-  }
-  // const weatherIcon = "http://openweathermap.org/img/w/";
 
-  useEffect(() => {
-    getData();
-  }, []);
+ const App = () => {
+ 
   return (
     <div className="App">
-      <div className="card">
-        <h2 className="title">{weatherdata.name} | {weatherdata.sys.country}</h2>
-        {/* <div className="search-form">
-          <input type="text" onChange={(e) => setCity(e.target.value)} />
-          <button type="button" onClick={() => getData()}>Search</button>
-        </div> */}
-        {weatherdata !== null ? (
-          <div className="main-container">
-            <h4>Live Weather condi</h4>
-            <h3>{weatherdata.weather[0].main}</h3>
-          <div className="temp">
-            <h4>{weatherdata.main.temp}</h4>
-          </div>
-          <div className="location">
-            <h3>Zoetermeer | Nederland</h3>
-          </div>
-          <div className="temp-range">
-            <h6>min: 25&deg;C || Max: 28&deg;C || Humidity: 10%</h6>
-          </div>
-      </div>
-        ): null}
-        
-      </div>
+
+      <WeatherCard city="Istanbul"/>
+      <WeatherCard city="Amsterdam"/>
+
     </div>
    
   );
 }
 export default App;
+
+//  <div className="search-form">
+// <input type="text" onChange={(e) => setCity(e.target.value)} />
+// <button type="button" onClick={() => getData()}>Search</button>
+// </div> 
--- React/react-projectguide/src/Components/WeatherInfo/WeatherInfo.js
+++ React/react-projectguide/src/Components/WeatherInfo/WeatherInfo.js
@@ -2,12 +2,13 @@ import axios from 'axios';
 
 const baseUrl = 'http://api.openweathermap.org/data/2.5/weather?';
 const apiKey = '17e4c2d50b76ef36977b9fd2b43d11d7';
+const units = '&units=metric';
 
 
 
 export const getWeatherData = async (cityname) => {
     try{
-        const {data} = await axios.get(baseUrl + `q=${cityname}&appid=${apiKey}`);
+        const {data} = await axios.get(baseUrl + `q=${cityname}&appid=${apiKey}${units}`);
         return data;
     }catch(error){
         throw error;
