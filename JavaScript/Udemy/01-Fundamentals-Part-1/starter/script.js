9be437944e0bd3ed808d757859f4ab2868c7b266
Future.Sayf <thefuture.sayf@gmail.com>
2021-04-25 18:07:55 +0200

    template strings

--- JavaScript/Udemy/01-Fundamentals-Part-1/starter/script.js
+++ JavaScript/Udemy/01-Fundamentals-Part-1/starter/script.js
@@ -1,5 +1,5 @@
-let js = 'amazing';
-console.log(5 + 5);
+// let js = 'amazing';
+/*console.log(5 + 5);
 
 console.log("abs");;
 console.log(23);
@@ -10,6 +10,64 @@ console.log(firstName);
 let country = "Holland";
 let continent = "Europa";
 let population = 18000000;
+*/
+// console.log(country, continent, population);
 
-console.log(country, continent, population);
+// Data Types
+
+/*let isIsland = "Holland";
+let language;
+
+isIsland = false;
+console.log(isIsland, language);
+*/
+
+// Let, const amd var
+// lecture Basic operator
+
+/*let population = 18;
+const halfPop = population / 2;
+console.log(halfPop);
+
+population++;
+console.log(population);
+
+const Finland = 6;
+
+console.log(population > Finland);
+console.log(population < 33);
+const description = "population is " + population + "and its " + population;
+console.log(description);
+*/
+
+//Operator Precedence
+//Coding challange #1
+/*const markHeight = 1.69;
+const markWeight = 78;
+
+const johnHeight = 1.95;
+const johnWeight = 92;
+
+const markBMI = markWeight / markHeight ** 2;
+console.log(markBMI);
+const johnBMI = johnWeight / johnHeight ** 2;
+console.log(johnBMI);
+
+const markHigerBMI = markBMI > johnBMI;
+console.log(markHigerBMI);
+
+Great Succes!
+*/
+
+const birthYear = 2031;
+
+let century;
+if (birthYear <= 2000) {
+    console.log(`This is the ${century = 20}`);
+} else {
+    console.log(`Dit is niet de ${century = 20}`);
+}
+
+console.log(century);
+
 
