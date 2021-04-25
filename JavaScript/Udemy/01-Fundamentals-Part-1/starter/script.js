ab095ef5677bf8abbaf82074c366c03ada5d344a
Future.Sayf <thefuture.sayf@gmail.com>
2021-04-25 15:07:11 +0200

    Assignment 1 for babies variables: udemy

--- JavaScript/Udemy/01-Fundamentals-Part-1/starter/script.js
+++ JavaScript/Udemy/01-Fundamentals-Part-1/starter/script.js
@@ -7,3 +7,9 @@ console.log(23);
 let firstName = "Jonas";
 console.log(firstName);
 
+let country = "Holland";
+let continent = "Europa";
+let population = 18000000;
+
+console.log(country, continent, population);
+
